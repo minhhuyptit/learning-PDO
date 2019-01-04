619e54151720d01121ae9b28badece98149c99a4
huy <minhhuy97.ptit@gmail.com>
2019-01-04 21:34:18 +0700

    Finish Learn PDO Basic

---
+++ study-pdo/fetch-fetchAll-query-execute-exec.php
@@ -0,0 +1,17 @@
+<?php
+    // fetch(): Kết quả trả vể chỉ có 1 dòng dữ liệu, đó là kết quả kế tiếp trong kết quả result
+    
+    // fetchAll(): kết quả trả về nhiều dòng dữ liệu, tất cả các kết quả có result
+     
+    // Query($statement): Thực thi truy vấn, trả về kết quả là 1 result set và false khi lỗi xảy ra
+    
+    // Execute(array input_param): Thực thi câu truy vấn của prepare, trả về true or false, Khi có tham số
+    // truyền vào sử dụng bindParam bindValue
+    
+    // Exec($statement): Thực thi câu lệnh SQL và trả về số lượng affectRow
+
+    // bindParam: khi bind sẽ lấy giá trị của biến nằm sau nó mà thực thi (nhận vào 1 biến nhưng ko có giá trị)
+    
+    // bindValue: khi bind sẽ lấy giá trị của biến nằm trước nó mà thực thi (nhận vào 1 biến có giá trị)
+
+?>
